577a120a74a89bc7fd1521b48323c4b2678714b1
NimaShariatz <shariatz@ualberta.ca>
2025-05-12 03:42:17 -0600

    map setup. image sizes set on load.

--- src/pages/map/map.tsx
+++ src/pages/map/map.tsx
@@ -1,5 +1,5 @@
 
-import {useState} from "react"
+import {useState, useEffect, useRef} from "react"
 
 import { starting_map } from "../../static/constants.tsx"
 import "./map.css"
@@ -7,6 +7,22 @@ import "./map.css"
 
 
 function Map() {
+    const [imageDimensions, setImageDimensions] = useState({ width: 0, height: 0 });
+    const mapRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        // Create a new Image object to get the dimensions
+        const img = new Image();
+        img.src = starting_map;
+        
+        // When the image loads, get its dimensions
+        img.onload = () => {
+            setImageDimensions({
+                width: img.width,
+                height: img.height
+            });
+        };
+    }, []);
 
 
 
@@ -14,21 +30,20 @@ function Map() {
     return (
         <>
             <div 
-                
+                ref={mapRef}
                 className="map_background" 
                 style={{ 
-                    backgroundImage: `url(${starting_map})`
+                    backgroundImage: `url(${starting_map})`,
+                    width: `${imageDimensions.width}px`,
+                    height: `${imageDimensions.height}px`
 
                 }}
-
             >
             </div>
-        
 
 
         </>
     )
-
 
 }
 export default Map
