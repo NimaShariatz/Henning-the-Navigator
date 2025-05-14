21390ee4bc8748916479744ae3b484b4e4c951e6
NimaShariatz <shariatz@ualberta.ca>
2025-05-14 01:42:00 -0600

    minimap setup

--- src/components/minimap.tsx
+++ src/components/minimap.tsx
@@ -1,12 +1,21 @@
+import {useRef} from "react"
+import "./minimap.css"
 
-
+import { starting_map } from "../static/constants"
 
 function Minimap() {
+
+    
+
+
     return(
         <>
-            <p>Yes</p>
+            <div className="minimap_container">
+                <img className="minimap_image" src={starting_map} />
+                <div className="minimap_block"></div>
+            </div>
         </>
     )
-
 }
+
 export default Minimap
