794d161bdebcaa738bf5987f3850637e2eaac882
NimaShariatz <shariatz@ualberta.ca>
2025-05-18 17:48:36 -0600

    more minimap buttons setup

--- src/components/minimap.tsx
+++ src/components/minimap.tsx
@@ -40,7 +40,6 @@ function Minimap({ on_minimap_click }: MinimapProps) {
 
             <div className="navigation_button_set">
 
-
                 <button className="minimap_button">
                     <svg xmlns="http://www.w3.org/2000/svg" width="85%" height="85%" viewBox="0 0 24 24">
                         <defs>
@@ -55,8 +54,6 @@ function Minimap({ on_minimap_click }: MinimapProps) {
                         <path className="navigation_waypoint" d="M0 0h24v24H0z" mask="url(#point)" />
                     </svg>
                 </button>
-
-
 
                <button className="minimap_button">
                     <svg xmlns="http://www.w3.org/2000/svg" width="85%" height="85%" viewBox="0 0 24 24">
@@ -73,8 +70,6 @@ function Minimap({ on_minimap_click }: MinimapProps) {
                     </svg>
                 </button>
 
-
-
                <button className="minimap_button">
                     <svg xmlns="http://www.w3.org/2000/svg" width="85%" height="85%" viewBox="0 0 24 24">
                         <defs>
@@ -89,8 +84,6 @@ function Minimap({ on_minimap_click }: MinimapProps) {
                         <path className="start_waypoint" d="M0 0h24v24H0z" mask="url(#point)" />
                     </svg>
                 </button>
-
-
 
                <button className="minimap_button">
                     <svg xmlns="http://www.w3.org/2000/svg" width="85%" height="85%" viewBox="0 0 24 24">
@@ -107,9 +100,26 @@ function Minimap({ on_minimap_click }: MinimapProps) {
                     </svg>
                 </button>
 
+                <button className="minimap_button">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="85%" height="85%" viewBox="0 0 24 24">
+                        <defs>
+                            <mask id="point">
+                                <g fill="none">
+                                    <path stroke="#ffffff" strokeLinecap="round" strokeOpacity="0.75" d="M19.361 18c.746.456 1.139.973 1.139 1.5s-.393 1.044-1.139 1.5s-1.819.835-3.111 1.098s-2.758.402-4.25.402s-2.958-.139-4.25-.402S5.385 21.456 4.639 21S3.5 20.027 3.5 19.5s.393-1.044 1.139-1.5" />
+                                    <path fill="#fff" fillOpacity="0.35" d="M19 10c0 5.016-5.119 8.035-6.602 8.804a.86.86 0 0 1-.796 0C10.119 18.034 5 15.016 5 10a7 7 0 0 1 14 0" />
+                                    <circle cx="12" cy="10" r="3" fill="#fff" />
+                                </g>
+                            </mask>
+                        </defs>
+                        <path className="target_extract" d="M0 0h24v24H0z" mask="url(#point)" />
+                    </svg>
+                </button>
 
-                
+            </div>
 
+
+            <div className="misc_button_set">
+                <p>s</p>
             </div>
 
 
