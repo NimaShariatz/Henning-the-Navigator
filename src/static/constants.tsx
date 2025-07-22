cb370133fd2b71548a0c174193e63ae651a55e81
NimaShariatz <shariatz@ualberta.ca>
2025-07-22 02:51:01 -0600

    file relocation

--- src/static/constants.tsx
+++ src/static/constants.tsx
@@ -1,20 +1,16 @@
 
 //maps
-export const
-
-
-
-Arras = new URL('../images/Arras.png', import.meta.url).href,
-Kuban = new URL('../images/Kuban.png', import.meta.url).href,
-Lapino = new URL('../images/Lapino.png', import.meta.url).href,
-Moscow = new URL('../images/Moscow.png', import.meta.url).href,
-Normandy = new URL('../images/Normandy.png', import.meta.url).href,
-Novosokolniki = new URL('../images/Novosokolniki.png', import.meta.url).href,
-Prokhorovka = new URL('../images/Prokhorovka.png', import.meta.url).href,
-Rheinland = new URL('../images/Rheinland.png', import.meta.url).href,
-Stalingrad = new URL('../images/Stalingrad.png', import.meta.url).href,
-Vluki = new URL('../images/Vluki.png', import.meta.url).href,
-Western_front = new URL('../images/Western_front.png', import.meta.url).href;
+export const Arras = "/Navigator/Arras.png";
+export const Kuban = "/Navigator/Kuban.png";
+export const Lapino = "/Navigator/Lapino.png";
+export const Moscow = "/Navigator/Moscow.png";
+export const Normandy = "/Navigator/Normandy.png";
+export const Novosokolniki = "/Navigator/Novosokolniki.png";
+export const Prokhorovka = "/Navigator/Prokhorovka.png";
+export const Rheinland = "/Navigator/Rheinland.png";
+export const Stalingrad = "/Navigator/Stalingrad.png";
+export const Vluki = "/Navigator/Vluki.png";
+export const Western_front = "/Navigator/Western_front.png";
 
 
 
