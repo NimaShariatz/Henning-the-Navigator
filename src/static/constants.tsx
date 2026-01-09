2b0e145736d32a3fd7e2a70a5d4e5b859f2d7c92
NimaShariatz <shariatz@ualberta.ca>
2026-01-09 00:42:05 -0700

    vite config base change to fix 404 error with new domain

--- src/static/constants.tsx
+++ src/static/constants.tsx
@@ -1,18 +1,18 @@
 
 //maps
-export const Arras = "/Henning-the-Navigator/Arras.webp";
-export const Kuban = "/Henning-the-Navigator/Kuban.webp";
-export const Lapino = "/Henning-the-Navigator/Lapino.webp";
-export const Moscow = "/Henning-the-Navigator/Moscow.webp";
-export const Normandy = "/Henning-the-Navigator/Normandy.jpg";
-export const Novosokolniki = "/Henning-the-Navigator/Novosokolniki.webp";
-export const Prokhorovka = "/Henning-the-Navigator/Prokhorovka.jpg";
-export const Rheinland = "/Henning-the-Navigator/Rheinland.jpg";
-export const Stalingrad = "/Henning-the-Navigator/Stalingrad.jpg";
-export const Vluki = "/Henning-the-Navigator/Vluki.jpg";
+export const Arras = "/Arras.webp";
+export const Kuban = "/Kuban.webp";
+export const Lapino = "/Lapino.webp";
+export const Moscow = "/Moscow.webp";
+export const Normandy = "/Normandy.jpg";
+export const Novosokolniki = "/Novosokolniki.webp";
+export const Prokhorovka = "/Prokhorovka.jpg";
+export const Rheinland = "/Rheinland.jpg";
+export const Stalingrad = "/Stalingrad.jpg";
+export const Vluki = "/Vluki.jpg";
 
-export const Western_front = "/Henning-the-Navigator/Western_front.jpg";
-export const Henning_logo = "/Henning-the-Navigator/Henning_logo.webp";
+export const Western_front = "/Western_front.jpg";
+export const Henning_logo = "/Henning_logo.webp";
 
 export const button_viewWidth_size = 0.016;
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+//  base: '/Henning-the-Navigator/',
 export default defineConfig({
   plugins: [react()],
-  base: '/Henning-the-Navigator/',
+  base: '/',
   build: {
     outDir: 'dist'
   }
