d9aa5965b19400d28870be9368b6a6b171e35240
NimaShariatz <shariatz@ualberta.ca>
2026-01-09 22:58:33 -0700

    map update

--- src/static/constants.tsx
+++ src/static/constants.tsx
@@ -1,13 +1,13 @@
 
 //maps
 export const Arras = "/Arras.webp";
-export const Kuban = "/Kuban.webp";
+export const Kuban = "/Kuban.webp";//
 export const Lapino = "/Lapino.webp";
-export const Moscow = "/Moscow.webp";
+export const Moscow = "/Moscow.webp";//
 export const Normandy = "/Normandy.jpg";
 export const Novosokolniki = "/Novosokolniki.webp";
-export const Prokhorovka = "/Prokhorovka.jpg";
-export const Rheinland = "/Rheinland.jpg";
+export const Prokhorovka = "/Prokhorovka.webp";
+export const Rheinland = "/Rheinland.webp";
 export const Stalingrad = "/Stalingrad.jpg";
 export const Vluki = "/Vluki.jpg";
 
